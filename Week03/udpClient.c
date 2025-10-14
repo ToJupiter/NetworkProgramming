1d32744beaa76ecc1221b7031787913474b11637
ToJupiter <hai.ph225715@sis.hust.edu.vn>
2025-10-14 17:44:29 +0700

    Week03 goes on

---
+++ Week03/udpClient.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define SERV_PORT 9000
+#define MAXLINE 1000000007
+
+int main()
+{
+  int sockfd, n, from_len;
+  struct sockaddr_in servaddr, from_socket;
+ socklen_t addrlen = sizeof(from_socket);
+// char sendline[MAXLINE], recvline[MAXLINE + 1];
+char *sendline = (char*) malloc(MAXLINE * sizeof(char));
+char *recvline = (char*) malloc((MAXLINE + 1) * sizeof(char));
+FILE *fp;
+
+servaddr.sin_family = AF_INET; 
+servaddr.sin_port = htons(SERV_PORT); 
+servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+//inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr); 
+
+ sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  printf("create socket\n");
+
+  while (fgets(sendline, MAXLINE, stdin) != NULL) { 
+  printf("To Server: %s", sendline);
+   sendto(sockfd, sendline, strlen(sendline), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
+   n = recvfrom(sockfd, recvline, MAXLINE, 0,  (struct sockaddr *) &from_socket, &addrlen);
+   recvline[n] = 0; //null terminate  
+   printf("From Server: %s %d %s", inet_ntoa(from_socket.sin_addr), htons(from_socket.sin_port), recvline);
+   } 
+}
