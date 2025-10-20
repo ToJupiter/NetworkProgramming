83b810ac78f297c2f2575505b45b0ad51fade8e3
ToJupiter <hai.ph225715@sis.hust.edu.vn>
2025-10-20 23:59:43 +0700

    tcpserver almost done

--- Week04/Bai01/tcpAppServer.c
+++ Week04/Bai01/tcpAppServer.c
@@ -6,6 +6,8 @@
 
 #define MAXLINE 1024
 #define EXIT_FAILURE 1
+#define LISTENQ 8
+#define SERV_PORT 9000
 
 void process_string(const char* input, char *letters, char *digits){
     int i = 0, l = 0, d = 0;
@@ -28,6 +30,8 @@ void process_string(const char* input, char *letters, char *digits){
 int main(int argc, char **argv){
     int sockfd;
     struct sockaddr_in servaddr, cliaddr;
+    socklen_t clilen;
+    int listenfd, connfd, n;
     char *buffer = (char*) malloc(MAXLINE * sizeof(char));
     char *letters = (char*) malloc(MAXLINE * sizeof(char));
     char *digits = (char*) malloc(MAXLINE * sizeof(char));
@@ -38,6 +42,33 @@ int main(int argc, char **argv){
     }
 
     int port = atoi(argv[1]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    listen(listenfd, LISTENQ);
+    printf("%s\n", "Server running... waiting for connections.");
+
+    for ( ; ; ){
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen);
+        printf("%s\n", "Received request...");
+        while((n = recv(connfd, buffer, MAXLINE, 0)) > 0){
+            process_string(&buffer, &letters, &digits);
+            printf("%s","String received from and resent to the client:");
+            puts(buffer);
+            
+            if (digits[0] != '\0'){
+                printf("Resending the digits: \n");
+                send(connfd, digits, strlen(digits), 0);
+                printf("Resending the letters: \n");
+                send(connfd, letters, strlen(letters), 0);
+            }
+        }
+    }
+
 }
 
