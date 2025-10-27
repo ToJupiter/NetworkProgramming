9d3bec69d6379818b4ea128931efa84f83346557
ToJupiter <hai.ph225715@sis.hust.edu.vn>
2025-10-28 05:48:11 +0700

    Hello world

--- Week041/appProtocolClient.c
+++ Week041/appProtocolClient.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <sys/socket.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/types.h>
+
+#define PORT 10002
+#define BUFFER_SIZE 4096
+
+typedef struct {
+    uint8_t type;
+    uint16_t length;
+    char data[BUFFER_SIZE];
+} Packet;
+
+void send_packet(int sock, uint8_t type, const char *data) {
+    uint16_t data_len = strlen(data);
+    uint8_t header[3];
+    header[0] = type;
+    *(uint16_t*)(header + 1) = htons(data_len);
+    
+    send(sock, header, 3, 0);
+    if(data_len > 0) {
+        send(sock, (void*)data, data_len, 0);
+    }
+}
+
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    
+    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    char buffer[BUFFER_SIZE];
+    
+    printf("Enter username: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+    send_packet(sock, 0x01, buffer);
+    
+    Packet response;
+    recv(sock, &response, 3, MSG_WAITALL);
+    
+    if(response.type == 0x81 && response.data[0] == 0x00) {
+        printf("Login successful!\n");
+        
+        while(1) {
+            printf("Enter message (or 'quit' to exit): ");
+            fgets(buffer, BUFFER_SIZE, stdin);
+            buffer[strcspn(buffer, "\n")] = 0;
+            
+            if(strcmp(buffer, "quit") == 0) break;
+            
+            send_packet(sock, 0x02, buffer);
+            recv(sock, &response, 3, MSG_WAITALL);
+        }
+    } else {
+        printf("Login failed!\n");
+    }
+    
+    close(sock);
+    return 0;
+}
--- Week041/appProtocolServer.c
+++ Week041/appProtocolServer.c
@@ -1,9 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <sys/socket.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/types.h>
 
+#define PORT 10002
+#define BUFFER_SIZE 4096
+
+typedef struct {
+    uint8_t type;
+    uint16_t length;
+    char data[BUFFER_SIZE];
+} Packet;
+
+void handle_login(int client_sock, Packet *packet, char *username) {
+    strncpy(username, packet->data, packet->length);
+    username[packet->length] = '\0';
+    
+    Packet response = {0x81, 1, {0x00}};
+    send(client_sock, &response, 3, 0);
+    printf("User '%s' logged in\n", username);
+}
+
+void handle_message(int client_sock, Packet *packet, const char *username) {
+    if(strlen(username) == 0) {
+        Packet response = {0x82, 1, {0x01}};
+        send(client_sock, &response, 3, 0);
+        return;
+    }
+    
+    char filename[256];
+    snprintf(filename, sizeof(filename), "%s.log", username);
+    
+    FILE *file = fopen(filename, "a");
+    if(file) {
+        packet->data[packet->length] = '\0';
+        fprintf(file, "%s\n", packet->data);
+        fclose(file);
+    }
+    
+    Packet response = {0x82, 1, {0x00}};
+    send(client_sock, &response, 3, 0);
+    printf("Message saved to %s\n", filename);
+}
+
+int main() {
+    int server_fd, client_sock;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 3);
+    
+    printf("Server listening on port %d\n", PORT);
+    
+    client_sock = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    
+    char username[256] = "";
+    
+    while(1) {
+        uint8_t header[3];
+        int bytes_read = recv(client_sock, header, 3, MSG_WAITALL);
+        
+        if(bytes_read <= 0) break;
+        
+        uint8_t type = header[0];
+        uint16_t length = ntohs(*(uint16_t*)(header + 1));
+        
+        Packet packet;
+        packet.type = type;
+        packet.length = length;
+        
+        if(length > 0) {
+            recv(client_sock, packet.data, length, MSG_WAITALL);
+        }
+        
+        switch(packet.type) {
+            case 0x01:
+                handle_login(client_sock, &packet, username);
+                break;
+            case 0x02:
+                handle_message(client_sock, &packet, username);
+                break;
+        }
+    }
+    
+    close(client_sock);
+    close(server_fd);
+    return 0;
+}
