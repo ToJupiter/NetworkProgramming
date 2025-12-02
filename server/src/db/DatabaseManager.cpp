0b3a51d387a776538e6d71fc8f70fed4e89a5024
ToJupiter <hai.ph225715@sis.hust.edu.vn>
2025-12-02 23:21:03 +0700

    seems to be working fine

--- server/src/db/DatabaseManager.cpp
+++ server/src/db/DatabaseManager.cpp
@@ -1,6 +1,7 @@
 #include "DatabaseManager.h"
 #include <iostream>
 #include <functional>
+#include "bcrypt.h"
 
 DatabaseManager& DatabaseManager::getInstance() {
     static DatabaseManager instance;
@@ -25,12 +26,13 @@ StatusCode DatabaseManager::registerUser(const std::string& email, const std::st
         *db << "SELECT COUNT(*) FROM users WHERE email = ?" << email >> count;
         if (count > 0) return StatusCode::EMAIL_EXISTS;
 
-        std::hash<std::string> hasher;
-        size_t hashed_val = hasher(password);
-        std::string hashed = std::to_string(hashed_val);
+        char salt[BCRYPT_HASHSIZE];
+        char hash[BCRYPT_HASHSIZE];
+        bcrypt_gensalt(12, salt);
+        bcrypt_hashpw(password.c_str(), salt, hash);
 
         *db << "INSERT INTO users (email, display_name, hashed_password) VALUES (?, ?, ?)"
-            << email << displayName << hashed;
+            << email << displayName << hash;
 
         return StatusCode::SUCCESS;
     } catch (const std::exception&) {
@@ -41,13 +43,9 @@ StatusCode DatabaseManager::registerUser(const std::string& email, const std::st
 std::optional<UserData> DatabaseManager::loginUser(const std::string& email, const std::string& password) {
     std::lock_guard<std::mutex> lock(dbMutex);
     try {
-        std::hash<std::string> hasher;
-        size_t hashed_val = hasher(password);
-        std::string hashed = std::to_string(hashed_val);
-        
+        std::string dbHash;
         uint32_t id = 0;
         std::string dbInfoName;
-        std::string dbPass;
         bool found = false;
 
         *db << "SELECT id, display_name, hashed_password FROM users WHERE email = ?" 
@@ -55,12 +53,15 @@ std::optional<UserData> DatabaseManager::loginUser(const std::string& email, con
             >> [&](uint32_t u_id, std::string d_name, std::string h_pass) {
                 id = u_id;
                 dbInfoName = d_name;
-                dbPass = h_pass;
+                dbHash = h_pass;
                 found = true;
             };
 
-        if (found && dbPass == hashed) {
-            return UserData{id, email, dbInfoName};
+        if (found) {
+            int password_match = bcrypt_checkpw(password.c_str(), dbHash.c_str());
+            if (password_match == 0){
+                return UserData{id, email, dbInfoName};
+            }
         }
     } catch (const std::exception& e) {
         std::cerr << "Login Error: " << e.what() << std::endl;
